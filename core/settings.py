303078362283a8424a59677aadd27f8cceae2bbb
bharathvenkatadhiri <bharathvenkatadhiri@gmail.com>
2024-08-22 20:59:40 +0530

    Fixed issue in base.html related to java script

--- core/settings.py
+++ core/settings.py
@@ -54,8 +54,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'invi',
+        'USER': 'root',
+        'PASSWORD': 'Haripriya!061298',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
